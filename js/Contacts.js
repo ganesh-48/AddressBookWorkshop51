520babcf30cf73582701b4116a3351296768c744
ganesh-48 <ganeshsrananavare@gmail.com>
2021-05-21 00:23:29 +0530

    ADDED: Input fields validation

---
+++ js/Contacts.js
@@ -0,0 +1,47 @@
+class Contacts {
+
+    get fullName() {return this._fullName;}
+    set fullName(fullName) {
+        let fullNamePattern = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if (fullNamePattern.test(fullName)) {
+            this._fullName = fullName;
+        }
+        else
+            throw 'Invalid Full Name';
+    }
+
+    get phonenumber() {return this._phonenumber;}
+    set phonenumber(phonenumber) {
+        let phonenumberPattern = RegExp('^((\\+)?(\\d{2}[-]))?(\\d{10}){1}?$');
+        if (phonenumberPattern.test(phonenumber)) {
+            this._phonenumber = phonenumber;
+        }
+        else
+            throw 'Invalid Phone Number';
+    }
+
+    get address() { return this._address; }
+    set address(address) {
+        let words = address.split(" ");
+        let addressPattern = RegExp('([A-Z a-z 0-9]{3,})+');
+        for (const word of words) {
+            if (!addressPattern.test(word))
+                throw 'Invalid Address';
+        }
+        this._address = address;
+    }
+
+    get city() {return this._city;}
+    set city(city) {this._city = city;}
+
+    get state() {return this._state;}
+    set state(state) {this._state = state;}
+
+    get zip() {return this._zip;}
+    set zip(zip) {this._zip = zip;}
+
+    
+    toString() {
+        return '[ FullName : ' + this.fullName + ' Address : '+ this.address + ' City : ' + this.city + ' State : ' + this.state + ' Zip : ' + this.zip +' Phone Number : ' + this.phoneNumber + ' ]';
+    }
+}
